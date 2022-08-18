32b54d233e8e4ec52940dc6c9ea6102c4ac94905
Colin Platt <colingplatt@gmail.com>
2022-08-18 12:39:53 +0200

    fixing approve and adding tests

--- src/DogPound.sol
+++ src/DogPound.sol
@@ -33,7 +33,7 @@ contract DogPound is Owned(msg.sender), ReentrancyGuard {
     uint256 constant RATE = 5_000_000_000; //
     uint256 constant MAX_CLAIM = 100 * 10**18; //maximum number of CANTO to be received
 
-    uint256 cINU_REMAINING;
+    uint256 public cINU_REMAINING;
 
     IERC20 public immutable cInu;
     IRouter public DEXRouter;
@@ -56,22 +56,10 @@ contract DogPound is Owned(msg.sender), ReentrancyGuard {
 
         require((claims[msg.sender]+amt_to_release) <= RATE * MAX_CLAIM, "ALREADY_CLAIMED"); // make sure that the user has not already claimed more than 5bn cINU from this address
 
-        cINU_REMAINING -= amt_to_release;
-        claims[msg.sender] += amt_to_release; // increase the counter for the amount being released in this transaction
-
-        cInu.transfer(msg.sender, amt_to_release); // transfer the cINU to the caller's address
-    }
-
-    function claim() external payable nonReentrant() {
-        require(msg.value <= MAX_CLAIM, "SENT_TOO_MUCH"); // user cannot send more than 100 CANTO
-
-        uint256 amt_to_release = msg.value * RATE; // calculate how much cINU is to be sent
-        require(cINU_REMAINING >= amt_to_release, "INSUFFICIENT_CANTO_INU"); // make sure that there is enough cINU in this contract
-
-        require((claims[msg.sender]+amt_to_release) <= RATE * MAX_CLAIM, "ALREADY_CLAIMED"); // make sure that the user has not already claimed more than 5bn cINU from this address
-
-        cINU_REMAINING -= amt_to_release;
-        claims[msg.sender] += amt_to_release; // increase the counter for the amount being released in this transaction
+        unchecked{
+            cINU_REMAINING = cINU_REMAINING - amt_to_release;
+            claims[msg.sender] += amt_to_release; // increase the counter for the amount being released in this transaction
+        }
 
         cInu.transfer(msg.sender, amt_to_release); // transfer the cINU to the caller's address
     }
@@ -83,6 +71,8 @@ contract DogPound is Owned(msg.sender), ReentrancyGuard {
     function fillLP() public onlyOwner {
         require(address(DEXRouter) != address(0), "ROUTER_UNSET");
 
+        cInu.approve(address(DEXRouter), cInu.balanceOf(address(this)));
+
         (uint amountToken, uint amountCANTO, uint liquidity) = DEXRouter.addLiquidityCANTO{value: address(this).balance}(address(cInu), false, cInu.balanceOf(address(this)),cInu.balanceOf(address(this)), address(this).balance, address(this), block.timestamp+3600);
 
         emit LPAdded(amountToken, amountCANTO, liquidity);
@@ -91,6 +81,8 @@ contract DogPound is Owned(msg.sender), ReentrancyGuard {
 
     function fillLP(uint256 cantoAmt, uint256 cInuAmt, uint256 cInuAmtMin) public onlyOwner {
         require(address(DEXRouter) != address(0), "ROUTER_UNSET");
+
+        cInu.approve(address(DEXRouter), cInuAmt);
 
         (uint amountToken, uint amountCANTO, uint liquidity) = DEXRouter.addLiquidityCANTO{value: cantoAmt}(address(cInu), false, cInuAmt, cInuAmtMin, cantoAmt, address(this), block.timestamp+3600);
 
--- test/DogPound.t.sol
+++ test/DogPound.t.sol
@@ -27,7 +27,7 @@ contract DogPoundTest is Test {
 
         payable(pound).call{gas: 100_000, value: 10*10**18}("");
 
-        //assertEq(cINU.balanceOf(address(pound)), 931_000_000_000_000 * 10**18);
+        assertEq(pound.cINU_REMAINING(), (500_000_000_000_000 * 10**18 - 50_000_000_000 * 10**18));
 
         assertEq(cINU.balanceOf(address(0xb0b)), 50_000_000_000 * 10**18);
     }
@@ -38,6 +38,32 @@ contract DogPoundTest is Test {
 
         payable(pound).call{gas: 100_000, value: 101*10**18}("");
 
+
+        assertEq(address(0xb0b).balance, 1000*10**18);
+        assertEq(address(pound).balance, 0);
+        assertEq(cINU.balanceOf(address(0xb0b)), 0);
+    }
+
+    //@TODO this not working, check math
+    function testReceiverExceedTotal() public {
+
+        unchecked{
+            for(uint160 i = 0; i<1000; i++){
+                vm.deal(address(i),1000*10**18);
+                vm.startPrank(address(i));
+                payable(pound).call{gas: 100_000, value: 100*10**18}("");
+                assertEq(pound.cINU_REMAINING(), (500_000_000_000_000 * 10**18 - ((i+1) * 500_000_000_000 * 10**18)));
+                vm.stopPrank();
+            }
+        }
+
+        emit log_uint(pound.cINU_REMAINING());
+        emit log_uint(cINU.balanceOf(address(pound)));
+
+        vm.deal(address(0xb0b),1000*10**18);
+        vm.startPrank(address(0xb0b));
+
+        payable(pound).call{gas: 100_000, value: 100*10**18}("");
 
         assertEq(address(0xb0b).balance, 1000*10**18);
         assertEq(address(pound).balance, 0);
